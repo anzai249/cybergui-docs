963a579e7a664f5abecdff279799619caaaef333
anzai249 <a381811169@163.com>
2023-10-24 00:57:35 +0800

    meta files and installation

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,35 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/anzai249/CyberGui',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/anzai249/cybergui-docs',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'CyberGui Docs',
   },
+  primaryHue: 271,
+  logo: (
+    <>
+      <svg width="50" height="50" xmlns="http://www.w3.org/4000/svg" version="1.1">
+        <g id="Gui">
+          <rect id="svg_1" height="40" width="7.5" y="5" x="21.25" stroke="#000" fill="#000000" />
+          <rect transform="rotate(120 25 25)" id="svg_3" height="40" width="7.5" y="5" x="21.25" stroke="#000" fill="#000000" />
+          <rect transform="rotate(60 25 25)" id="svg_4" height="40" width="7.5" y="5" x="21.25" stroke="#000" fill="#000000" />
+        </g>
+      </svg>
+      <span style={{ marginLeft: '.4em', fontWeight: 800 }}>CyberGui Docs</span>
+    </>
+  ),
+  i18n: [
+    { locale: 'en-US', text: 'English' },
+    { locale: 'zh-CN', text: '简体中文' }
+  ],
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s - CyberGui Docs',
+    }
+  }
 }
 
 export default config
