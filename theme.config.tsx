35e5f3f3c2ef0a8d65b9837f4f66c356c128e25b
anzai249 <a381811169@163.com>
2023-11-12 16:54:13 +0800

    New theme colours and settings, logo dark mode

--- theme.config.tsx
+++ theme.config.tsx
@@ -14,9 +14,9 @@ const config: DocsThemeConfig = {
     <>
       <svg width="50" height="50" xmlns="http://www.w3.org/4000/svg" version="1.1">
         <g id="Gui">
-          <rect id="svg_1" height="40" width="7.5" y="5" x="21.25" stroke="#000" fill="#000000" />
-          <rect transform="rotate(120 25 25)" id="svg_3" height="40" width="7.5" y="5" x="21.25" stroke="#000" fill="#000000" />
-          <rect transform="rotate(60 25 25)" id="svg_4" height="40" width="7.5" y="5" x="21.25" stroke="#000" fill="#000000" />
+          <rect id="svg_1" height="40" width="7.5" y="5" x="21.25" stroke="currentColor" fill="currentColor" />
+          <rect transform="rotate(120 25 25)" id="svg_3" height="40" width="7.5" y="5" x="21.25" stroke="currentColor" fill="currentColor" />
+          <rect transform="rotate(60 25 25)" id="svg_4" height="40" width="7.5" y="5" x="21.25" stroke="currentColor" fill="currentColor" />
         </g>
       </svg>
       <span style={{ marginLeft: '.4em', fontWeight: 800 }}>CyberGui Docs</span>
